7d2ee4da6a7a890b8b7c41e90a55f85d817234bc
EspenB08 <142892271+EspenB08@users.noreply.github.com>
2024-01-12 01:20:13 +0100

    added card one with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
 import "./index.css";
 
-
-
-function App() { 
-  return(
+function App() {
+  console.log("test");
+  return (
     <div className="App">
       <Avbar></Avbar>
+      <div className="cardContainer">
+        <div className="section1">
+          <Card1></Card1>
+          <Card1></Card1>
+          <Card1></Card1>
+        </div>
+        <div className="section2">
+          <Card1></Card1>
+          <Card1></Card1>
+          <Card1></Card1>
+        </div>
+      </div>
 
-
-    <main className="main">
-      <h2>GIVEN UP</h2>
-    </main>
-
-    <footer className="footer">
-      <h2>contact me</h2>
-    </footer>
-  
+      <main className="main">
+        <h2>GIVEN UP</h2>
+      </main>
+      <footer className="footer">
+        <h2>contact me</h2>
+      </footer>
     </div>
-  )
-  
+  );
+}
+function Card1() {
+  return (
+    <div className="Card1">
+      <div className="imgContainer">
+        <img src="" alt="test" />
+      </div>
+      <h1 className="text1">hey</h1>
+      <h2 className="text2">hei</h2>
+    </div>
+  );
 }
 
 function Avbar() {
-
   const givenUp = "kjøøøøøøøøøøtt";
 
   return (
@@ -33,3 +50,4 @@ function Avbar() {
 }
 
 export default App;
+// npm run dev (for å kjøre koden)
